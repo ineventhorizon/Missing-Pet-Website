7c0191d3d9bfa6a4f53a13d5fb5fddcb07601dfc
ineventhorizon <yusufekremgs@gmail.com>
2018-12-20 20:45:48 +0300

    Color palette updated

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const app=express();
 const indexRoutes=require("./routes/indexRoutes");
 const userRoutes=require("./routes/userRoutes");
 
-mongoose.connect('mongodb://kayip-dostlar:UzWIaJAGxWtf6EVY@cluster0-shard-00-00-c8zez.gcp.mongodb.net:27017,cluster0-shard-00-01-c8zez.gcp.mongodb.net:27017,cluster0-shard-00-02-c8zez.gcp.mongodb.net:27017/SiteDB?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true',{
+mongoose.connect('',{
     useCreateIndex: true,
     useNewUrlParser:true
     
